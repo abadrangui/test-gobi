a5c409cd262399360e47674bf9fbecd2a4825d08
Badrangui <abadrangui@gmail.com>
2020-07-26 16:38:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Carousel from './components/Carousel';
+import BigGrid from './components/BigGrid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Carousel />
+
+      <Container>
+        <Row>
+          <Col xs={9} lg={6} className="mx-auto my-5">
+            <p>LoremIpsum360 ° үнэ төлбөргүй онлайн генератор хуурамч текст юм. Энэ нь таны загварууд нь солих текст эсвэл өөр текст үүсгэхийн тулд иж бүрэн текст симулятор өгдөг. Энэ нь өөр өөр хэл дээр эх бичвэрийг жишээ бий болгох латин кирилл болон янз бүрийн санамсаргүй текстийг боломжуудтай байдаг.</p>
+          </Col>
+        </Row>
+      </Container>
+
+      <BigGrid />
     </div>
   );
 }
